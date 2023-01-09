db6b56b4514228ef898ace10015951ec3baec4aa
sm9171 <sm9171@nate.com>
2023-01-09 10:40:13 +0900

    init

---
+++ src/test/java/com/sm9171/carrotmarket/CarrotMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm9171.carrotmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
